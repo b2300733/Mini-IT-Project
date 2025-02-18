da4a76bb49957d55e10111aba16fc112673001c6
b2300733 <b2300733@helplive.edu.my>
2025-02-18 21:32:54 +0800

    update login and a little not important things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { JobsComponent } from './components/jobs/jobs.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { CommunitymarketComponent } from './components/communitymarket/communitymarket.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'market',
+    component: CommunitymarketComponent,
   },
 ];
 
