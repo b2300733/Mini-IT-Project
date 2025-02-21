1fe7835eaf86fa57a7c15b43741f2253b96b2a70
b2300733 <b2300733@helplive.edu.my>
2025-02-22 01:47:46 +0800

    LATEST

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -7,7 +7,7 @@ const User = require("../models/user.model");
 //   new FacebookStrategy(
 //     {
 //       clientID: "843271631263870",
-//       clientSecret: "",
+//       clientSecret: "{}",
 //       callbackURL: "http://localhost:3000/auth/facebook/callback",
 //       profileFields: ["id", "displayName", "photos", "email"],
 //     },
@@ -39,7 +39,7 @@ passport.use(
     {
       clientID:
         "814700521844-rf1kg5pq92r9t303u4vmr63qnsvqv0r4.apps.googleusercontent.com",
-      clientSecret: "",
+      clientSecret: "{}",
       callbackURL: "http://localhost:3000/auth/google/callback",
     },
     async (token, tokenSecret, profile, done) => {
