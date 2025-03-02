fbe3e906c77e5a36bf48c608fa051e0d4f1a8267
b2300733 <b2300733@helplive.edu.my>
2025-03-02 15:22:15 +0800

    remove clientID and Secret for now

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -72,9 +72,8 @@ const sendWelcomeEmail = async (email, username) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "814700521844-rf1kg5pq92r9t303u4vmr63qnsvqv0r4.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientID: "[]",
+      clientSecret: "[]",
       callbackURL: "http://localhost:3000/auth/google/callback",
     },
     async (token, tokenSecret, profile, done) => {
