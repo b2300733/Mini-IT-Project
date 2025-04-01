918dccba86a1fc955fb1da0f4213b31521a46232
b2300733 <b2300733@helplive.edu.my>
2025-04-01 16:23:57 +0800

    updated out of stock

--- src/app/components/shopproductpage/shopproductpage.component.ts
+++ src/app/components/shopproductpage/shopproductpage.component.ts
@@ -93,11 +93,20 @@ export class ShopproductpageComponent implements OnInit {
     }
   }
 
+  isProductInStock(): boolean {
+    return this.product.quantity > 0;
+  }
+
   addToCart(): void {
     if (!this.isLoggedIn()) {
       // Redirect to login page if not logged in
       alert('Please login first to sell items');
       this.router.navigate(['/login']);
+      return;
+    }
+
+    if (!this.isProductInStock()) {
+      alert('Sorry, this item is out of stock.');
       return;
     }
 
